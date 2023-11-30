ddb916fac02dda4ef1051141f1ecc1ae893117f3
Dalia Grodzka <39556362+NiyeSadece@users.noreply.github.com>
2023-11-30 11:24:10 +0100

    added serializers

---
+++ smakosz/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile, Reservation, Table
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+        ]
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = [
+            'user',
+            'first_name',
+            'last_name',
+            'phone_number',
+            'image',
+            'role',
+        ]
+
+
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Table
+        fields = [
+            'id',
+            'capacity',
+            'is_occupied',
+        ]
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    customer = ProfileSerializer(read_only=True)
+    table = TableSerializer(read_only=True)
+
+    class Meta:
+        model = Reservation
+        fields = [
+            'customer',
+            'table',
+            'date',
+            'how_many',
+            'is_confirmed',
+        ]
